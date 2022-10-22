b0aa0c3265e3bcf0fd5b5044e56124cff7caff72
s17476 <s17476@pjwstk.edu.pl>
2022-10-22 10:47:12 +0200

    assets page filtering

--- lib/features/assets/presentation/pages/assets_page.dart
+++ lib/features/assets/presentation/pages/assets_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:under_control_v2/features/assets/domain/entities/asset.dart';
-import 'package:under_control_v2/features/assets/presentation/widgets/asset_tile.dart';
-import 'package:under_control_v2/features/assets/utils/asset_status.dart';
 
 import '../../../core/utils/get_user_premission.dart';
 import '../../../core/utils/premission.dart';
 import '../../../core/utils/responsive_size.dart';
 import '../../../groups/domain/entities/feature.dart';
 import '../../../knowledge_base/presentation/widgets/shimmer_instruction_tile.dart';
-import '../../../user_profile/presentation/blocs/user_profile/user_profile_bloc.dart';
+import '../../domain/entities/asset.dart';
+import '../../utils/asset_status.dart';
 import '../blocs/asset/asset_bloc.dart';
 import '../blocs/asset_category/asset_category_bloc.dart';
+import '../widgets/asset_tile.dart';
 
 class AssetsPage extends StatefulWidget {
   const AssetsPage({
@@ -34,13 +33,17 @@ class _AssetsPageState extends State<AssetsPage> with ResponsiveSize {
   List<Asset>? _assets;
   List<Asset>? _filteredAssets;
   int _index = 0;
-  final List<Asset> _inUse = [];
-  final List<Asset> _inReserve = [];
+  List<Asset> _inUse = [];
+  List<Asset> _inReserve = [];
 
-  void _filterAssets(int index) {
-    _index = index;
-    print(_index);
-    switch (index) {
+  void _setIndex(int value) {
+    setState(() {
+      _index = value;
+    });
+  }
+
+  void _filterAssets() {
+    switch (_index) {
       case 0:
         _filteredAssets = _assets;
         break;
@@ -72,17 +75,11 @@ class _AssetsPageState extends State<AssetsPage> with ResponsiveSize {
         break;
     }
     if (_filteredAssets != null) {
-      for (var asset in _filteredAssets!) {
-        if (asset.isInUse) {
-          setState(() {
-            _inUse.add(asset);
-          });
-        } else {
-          setState(() {
-            _inReserve.add(asset);
-          });
-        }
-      }
+      _inUse.clear();
+      _inReserve.clear();
+
+      _inUse = _filteredAssets!.where((asset) => asset.isInUse).toList();
+      _inReserve = _filteredAssets!.where((asset) => !asset.isInUse).toList();
     }
   }
 
@@ -156,53 +153,57 @@ class _AssetsPageState extends State<AssetsPage> with ResponsiveSize {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       AnimatedContainer(
+                        color: Theme.of(context).appBarTheme.backgroundColor,
                         duration: const Duration(milliseconds: 300),
                         height: widget.isSearchBoxExpanded
                             ? widget.searchBoxHeight + 4
                             : 0,
                       ),
-                      TabBar(
-                        tabs: [
-                          Tab(
-                            icon: Text(
-                              AppLocalizations.of(context)!.all,
-                              style: TextStyle(
-                                color: tabBarIconColor,
-                                fontSize: 18,
+                      Container(
+                        color: Theme.of(context).appBarTheme.backgroundColor,
+                        child: TabBar(
+                          tabs: [
+                            Tab(
+                              icon: Text(
+                                AppLocalizations.of(context)!.all,
+                                style: TextStyle(
+                                  color: tabBarIconColor,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
-                          ),
-                          Tab(
-                            icon: Icon(
-                              Icons.add,
-                              color: tabBarIconColor,
-                              size: tabBarIconSize,
+                            Tab(
+                              icon: Icon(
+                                Icons.add,
+                                color: tabBarIconColor,
+                                size: tabBarIconSize,
+                              ),
                             ),
-                          ),
-                          Tab(
-                            icon: Icon(
-                              Icons.remove,
-                              color: tabBarIconColor,
-                              size: tabBarIconSize,
+                            Tab(
+                              icon: Icon(
+                                Icons.remove,
+                                color: tabBarIconColor,
+                                size: tabBarIconSize,
+                              ),
                             ),
-                          ),
-                          Tab(
-                            icon: Icon(
-                              Icons.compare_arrows,
-                              color: tabBarIconColor,
-                              size: tabBarIconSize,
+                            Tab(
+                              icon: Icon(
+                                Icons.compare_arrows,
+                                color: tabBarIconColor,
+                                size: tabBarIconSize,
+                              ),
                             ),
-                          ),
-                          Tab(
-                            icon: Icon(
-                              Icons.compare_arrows,
-                              color: tabBarIconColor,
-                              size: tabBarIconSize,
+                            Tab(
+                              icon: Icon(
+                                Icons.compare_arrows,
+                                color: tabBarIconColor,
+                                size: tabBarIconSize,
+                              ),
                             ),
-                          ),
-                        ],
-                        onTap: _filterAssets,
-                        indicatorColor: tabBarIconColor,
+                          ],
+                          onTap: _setIndex,
+                          indicatorColor: tabBarIconColor,
+                        ),
                       ),
                       // assets list
                       BlocBuilder<AssetBloc, AssetState>(
@@ -226,19 +227,7 @@ class _AssetsPageState extends State<AssetsPage> with ResponsiveSize {
                               state.allAssets.allAssets,
                               widget.searchQuery,
                             );
-                            print('jaki index');
-                            print(_index);
-                            if (_index == 0) {
-                              for (var asset in _assets!) {
-                                if (asset.isInUse) {
-                                  _inUse.add(asset);
-                                } else {
-                                  _inReserve.add(asset);
-                                }
-                              }
-                            }
-                            // TO DO
-                            print(_inUse);
+                            _filterAssets();
                             return Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
