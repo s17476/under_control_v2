95e687c97dbc9f94372633f8868678b6a7ae0cbc
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2023-01-25 10:08:27 +0100

    iOS notifications configuration

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Grzegorz Fraczek on 25/01/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
