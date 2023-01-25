e201ee308156017176473f87661756c1427e91c2
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2023-01-25 13:18:47 +0100

    Cloud Functions test

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.taskAdded = functions.firestore
+    .document("companies/{companyId}/tasks/{taskId}")
+    .onCreate((document, context) => {
+      const newTask = document.data();
+      console.log(newTask);
+
+      return;
+    });
