9c8c7b50585fac2123bf6b53543e276497a05257
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2023-01-30 12:20:20 +0100

    Notifications - new task function prototype added

--- functions/index.js
+++ functions/index.js
@@ -13,43 +13,92 @@ exports.taskAdded = functions.firestore
 
       const newTask = document.data();
 
-      let tokens = [];
+      // notification data
+      const text = newTask.title;
+      const payload = {
+        notification: {
+          title: `NEWTASK# ${document.id}`,
+          body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+        }
+      };
+      const dbPayload = {
+        'type': 'tasks',
+        'groupTask': true,
+        'title': 'NEWTASK#',
+        'id': document.id,
+      };
+
+      const tokens = [];
+      const userIds = [];
       
       // if task is assigned to a group
       if(newTask.assignedGroups.length != 0){
+        // get users assigned to a group task
         const users = await db
           .collection('users')
           .where('companyId', '==', companyId)
           .where('userGroups', 'array-contains-any', newTask.assignedGroups)
-
           .get();
 
-          if (users.empty) {
-            console.log('No matching documents.');
-            return;
-          }
+        if (users.empty) {
+          console.log('No matching documents.');
+          return;
+        }
 
-        users.forEach(async element => {
-          const taskUser = await db
+        
+        // get notifications permissions for each user
+        for(const user of users.docs){
+          const userPermissions = await db
             .collection('users')
-            .doc(element.id)
+            .doc(user.id)
             .collection('settings')
             .doc('notifications')
             .get();
 
-          console.log(taskUser.id, '=>', taskUser.data());
-          
-        });
+          // check user permissions
+          // if no permissions found, then default permission option is TRUE
+          if((userPermissions.exists && (userPermissions.data().tasks == true || userPermissions.data().tasks == undefined)) || !userPermissions.exists){
+            // add token to the list
+            const userTokens = user.data().deviceTokens;
 
-        // for(let user in users){
-        //   tokens = [...tokens, ...user.data.deviceTokens];
-        // }
+            userTokens.forEach(token => {
+              if(tokens.indexOf(token) === -1){
+                tokens.push(token);
+              }
+            });
+            
+            // users collection
+            if(userIds.indexOf(user.id) === -1){
+              userIds.push(user.id);
+            }
+          }
+        }
 
-        //   console.log(tokens);
       }
+      
+      console.log('tokens =>',tokens);
+      console.log('userIds =>',tokens);
 
+      for(const token of tokens){
+        console.log('sent notification to:', token);
+      }
+        
+      // add new notifications to the db
+      for(const userId of userIds){
+        await addNotification(userId, document.id, dbPayload);
 
-
+        console.log('Added to db');
+      }
+      await admin.messaging().sendToDevice(tokens, payload);
 
       return;
     });
+
+function addNotification(userId, documentId, dbPayplad){
+  return db
+  .collection('users')
+  .doc(userId)
+  .collection('notifications')
+  .doc(documentId)
+  .set(dbPayplad);
+}
