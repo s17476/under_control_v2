6871e33ee74331e10433d394693e331bf65b05b2
Grzegorz Fraczek <gf@lnasoftware.com>
2023-03-16 18:52:57 +0100

    NotApprovedPage - responsive design

--- lib/features/user_profile/presentation/pages/not_approved_page.dart
+++ lib/features/user_profile/presentation/pages/not_approved_page.dart
@@ -3,13 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../company_profile/presentation/blocs/company_profile/company_profile_bloc.dart';
-import '../../../core/utils/responsive_size.dart';
 import '../../../core/utils/show_snack_bar.dart';
 import '../../../core/utils/size_config.dart';
 import '../blocs/user_management/user_management_bloc.dart';
 import '../blocs/user_profile/user_profile_bloc.dart';
 
-class NotApprovedPage extends StatelessWidget with ResponsiveSize {
+class NotApprovedPage extends StatelessWidget {
   const NotApprovedPage({Key? key}) : super(key: key);
 
   @override
@@ -41,158 +40,188 @@ class NotApprovedPage extends StatelessWidget with ResponsiveSize {
           }
         },
         child: Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // image
-                Container(
-                  height: responsiveSizeVerticalPct(small: 40),
-                  padding: const EdgeInsets.all(32.0),
-                  child: Image.asset(
-                    'assets/validation.png',
-                    fit: BoxFit.scaleDown,
-                  ),
-                ),
-                // message and buttons
-                BlocBuilder<CompanyProfileBloc, CompanyProfileState>(
-                  builder: (context, state) {
-                    // check if there are other users
-                    bool isAdministrator = false;
-                    if (state is CompanyProfileLoaded &&
-                        state.companyUsers.activeUsers.isEmpty) {
-                      isAdministrator = true;
-                    }
-                    return SizedBox(
-                      height: responsiveSizeVerticalPct(small: 40),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            AppLocalizations.of(context)!.well_done,
-                            style: Theme.of(context).textTheme.headlineSmall,
-                          ),
-                          if (!isAdministrator)
-                            Text(
-                              AppLocalizations.of(context)!.approve_page_text,
-                              textAlign: TextAlign.justify,
-                            ),
-                          if (isAdministrator)
-                            Text(
-                              AppLocalizations.of(context)!
-                                  .approve_page_admin_text,
-                              textAlign: TextAlign.justify,
-                            ),
-                          if (!isAdministrator)
-                            ElevatedButton(
-                              onPressed: () =>
-                                  context.read<UserProfileBloc>().add(
-                                        GetUserByIdEvent(
-                                          userId: (context
-                                                  .read<UserProfileBloc>()
-                                                  .state as NotApproved)
-                                              .userProfile
-                                              .id,
-                                        ),
-                                      ),
-                              child: Text(
-                                AppLocalizations.of(context)!
-                                    .approve_page_change_button_refresh,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: SizedBox(
+                width: 700,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      // image
+                      Container(
+                        height: 250,
+                        padding: const EdgeInsets.all(32.0),
+                        child: Image.asset(
+                          'assets/validation.png',
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                      // message and buttons
+                      BlocBuilder<CompanyProfileBloc, CompanyProfileState>(
+                        builder: (context, state) {
+                          // check if there are other users
+                          bool isAdministrator = false;
+                          if (state is CompanyProfileLoaded &&
+                              state.companyUsers.activeUsers.isEmpty) {
+                            isAdministrator = true;
+                          }
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                AppLocalizations.of(context)!.well_done,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
                               ),
-                            ),
-                          if (isAdministrator)
-                            ElevatedButton(
-                              onPressed: () {
-                                context.read<UserManagementBloc>().add(
-                                      ApproveUserAndMakeAdminEvent(
-                                        userId: (context
-                                                .read<UserProfileBloc>()
-                                                .state as NotApproved)
-                                            .userProfile
-                                            .id,
-                                      ),
-                                    );
-                                context.read<UserProfileBloc>().add(
-                                      GetUserByIdEvent(
-                                        userId: (context
-                                                .read<UserProfileBloc>()
-                                                .state as NotApproved)
-                                            .userProfile
-                                            .id,
-                                      ),
-                                    );
-                              },
-                              child: Text(
-                                AppLocalizations.of(context)!.continue_btn,
+                              const SizedBox(
+                                height: 16,
                               ),
-                            ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.black),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  actionsAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  title: Text(
-                                    AppLocalizations.of(context)!
-                                        .assign_company_list_confirm_dialog_title,
-                                  ),
-                                  content: Text(
-                                    AppLocalizations.of(context)!
-                                        .approve_page_change_dialog_text,
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                      child: Text(
-                                        AppLocalizations.of(context)!.cancel,
-                                        style: TextStyle(
-                                          color: Theme.of(context)
-                                              .textTheme
-                                              .displayLarge!
-                                              .color,
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                    TextButton(
-                                      child: Text(
-                                        AppLocalizations.of(context)!.change,
-                                      ),
-                                      onPressed: () {
+                              if (!isAdministrator)
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .approve_page_text,
+                                  textAlign: TextAlign.justify,
+                                ),
+                              if (isAdministrator)
+                                Text(
+                                  AppLocalizations.of(context)!
+                                      .approve_page_admin_text,
+                                  textAlign: TextAlign.justify,
+                                ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              if (!isAdministrator)
+                                SizedBox(
+                                  width: 500,
+                                  child: ElevatedButton(
+                                    onPressed: () =>
                                         context.read<UserProfileBloc>().add(
-                                              ResetCompanyEvent(
-                                                userProfile: (context
+                                              GetUserByIdEvent(
+                                                userId: (context
                                                         .read<UserProfileBloc>()
                                                         .state as NotApproved)
-                                                    .userProfile,
+                                                    .userProfile
+                                                    .id,
                                               ),
-                                            );
-                                        Navigator.pop(context);
-                                      },
+                                            ),
+                                    child: Text(
+                                      AppLocalizations.of(context)!
+                                          .approve_page_change_button_refresh,
                                     ),
-                                  ],
+                                  ),
                                 ),
-                              );
-                            },
-                            child: Text(
-                              AppLocalizations.of(context)!
-                                  .approve_page_change_button,
-                            ),
-                          ),
-                        ],
+                              if (isAdministrator)
+                                SizedBox(
+                                  width: 500,
+                                  child: ElevatedButton(
+                                    onPressed: () {
+                                      context.read<UserManagementBloc>().add(
+                                            ApproveUserAndMakeAdminEvent(
+                                              userId: (context
+                                                      .read<UserProfileBloc>()
+                                                      .state as NotApproved)
+                                                  .userProfile
+                                                  .id,
+                                            ),
+                                          );
+                                      context.read<UserProfileBloc>().add(
+                                            GetUserByIdEvent(
+                                              userId: (context
+                                                      .read<UserProfileBloc>()
+                                                      .state as NotApproved)
+                                                  .userProfile
+                                                  .id,
+                                            ),
+                                          );
+                                    },
+                                    child: Text(
+                                      AppLocalizations.of(context)!
+                                          .continue_btn,
+                                    ),
+                                  ),
+                                ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              SizedBox(
+                                width: 500,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.black),
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        actionsAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(15),
+                                        ),
+                                        title: Text(
+                                          AppLocalizations.of(context)!
+                                              .assign_company_list_confirm_dialog_title,
+                                        ),
+                                        content: Text(
+                                          AppLocalizations.of(context)!
+                                              .approve_page_change_dialog_text,
+                                        ),
+                                        actions: [
+                                          TextButton(
+                                            child: Text(
+                                              AppLocalizations.of(context)!
+                                                  .cancel,
+                                              style: TextStyle(
+                                                color: Theme.of(context)
+                                                    .textTheme
+                                                    .displayLarge!
+                                                    .color,
+                                              ),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                          TextButton(
+                                            child: Text(
+                                              AppLocalizations.of(context)!
+                                                  .change,
+                                            ),
+                                            onPressed: () {
+                                              context
+                                                  .read<UserProfileBloc>()
+                                                  .add(
+                                                    ResetCompanyEvent(
+                                                      userProfile: (context
+                                                              .read<
+                                                                  UserProfileBloc>()
+                                                              .state as NotApproved)
+                                                          .userProfile,
+                                                    ),
+                                                  );
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    AppLocalizations.of(context)!
+                                        .approve_page_change_button,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
                       ),
-                    );
-                  },
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
           ),
         ),
